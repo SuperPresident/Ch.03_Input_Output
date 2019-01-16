6b01ee8c951b159ab6d105cd91dbb86a5f641b15
Superpresident <dariusbrowne12@gmail.com>
2019-01-16 14:06:10 -0600

    2 commit

--- leche.py
+++ leche.py
@@ -3,19 +3,31 @@ number=random.randrange(1,101)
 
 done=False
 count=0
-
+record=10000000000000000000000000000000000000000000000000
+print("this is a guess the computers numer game.")
 while not done:
+
+
     count+=1
+
     #ask a user for a number
-    guess=int(input("What is my number?"))
+    guess=input("Type a number or Q to quit\r")
+    if guess.lower()=="q":
+        done=True
+
 
     #see how the number compares to the computer
-    if guess>number:
+
+    elif int(guess)>number:
         print('Too high!')
 
-    elif guess<number:
+    elif int(guess)<number:
             print("too low!")
     else:
         print("congrats")
         print("it took you",count,"times")
-        done=True
+        if count<record:
+            record=count
+            print("your current record is",record,"times")
+            number=random.randrange(1,101)
+        count=0
